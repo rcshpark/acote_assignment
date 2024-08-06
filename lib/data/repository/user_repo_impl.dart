c492dc9f923ed91204ff181cfc7e00f3b58bc886
rcshpark <rcshpark@gmail.com>
2024-08-06 21:57:52 +0900

    feat : 특정 사용자 레파지토리 조회 data 영역 로직 구현

--- lib/data/repository/user_repo_impl.dart
+++ lib/data/repository/user_repo_impl.dart
@@ -1,7 +1,9 @@
 import 'package:acote_assignment/data/data_source/remote/user_data_source.dart';
+import 'package:acote_assignment/data/dto/repo_dto.dart';
 import 'package:acote_assignment/data/dto/user_dto.dart';
 import 'package:acote_assignment/data/translator/translator.dart';
 import 'package:acote_assignment/domain/model/data_state_model.dart';
+import 'package:acote_assignment/domain/model/repo_model.dart';
 import 'package:acote_assignment/domain/model/user_model.dart';
 import 'package:acote_assignment/domain/respository/user_repo.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -20,6 +22,17 @@ class UserRepoImpl implements UserRepo {
       final response = await ref.read(userDataSource).fetchUser(since);
       List<UserDto> userDto = response.map((e) => UserDto.fromJson(e)).toList();
       return Translator().transalateUserInfo(userDto);
+    } catch (error) {
+      return DataState.error(Exception(), error.toString());
+    }
+  }
+
+  @override
+  Future<DataState<List<RepoModel>>> fetchUserRepo(String userName) async {
+    try {
+      final response = await ref.read(userDataSource).fetchUserRepo(userName);
+      List<RepoDto> repoDto = response.map((e) => RepoDto.fromJson(e)).toList();
+      return Translator().transalateRepoInfo(repoDto);
     } catch (error) {
       return DataState.error(Exception(), error.toString());
     }
